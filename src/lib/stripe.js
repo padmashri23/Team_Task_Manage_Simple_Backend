7eec20217cdfd5b9269aaa66d740493d46a8d87c
Padmashri S <130970670+padmashri23@users.noreply.github.com>
2026-01-09 13:13:22 +0530

    Update stripe.js

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,6 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js'
 
-// Initialize Stripe with your publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY)
 
 export { stripePromise }
